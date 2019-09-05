7fb9140658827165c317f3512bd2833430443ca7
madalinaeleonorag <madalinaeleonora.gheorghe@gmail.com>
2019-09-05 21:50:52 +0300

    started to add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: 'AIzaSyCcTEDMXl2kbpnzoF_x5rwdnKJQP_rwD1U',
+  authDomain: 'universityraking.firebaseapp.com',
+  databaseURL: 'https://universityraking.firebaseio.com',
+  projectId: 'universityraking',
+  storageBucket: 'universityraking.appspot.com',
+  messagingSenderId: '709054246209'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +25,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
