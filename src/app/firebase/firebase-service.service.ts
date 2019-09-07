c22ffa70b8a136b4a9d522f90c42ac670bbb7186
madalinaeleonorag <madalinaeleonora.gheorghe@gmail.com>
2019-09-07 23:29:24 +0300

    started to add firebase

---
+++ src/app/firebase/firebase-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+}
