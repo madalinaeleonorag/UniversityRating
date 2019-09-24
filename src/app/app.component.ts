1c4881bc8c5e6b8e3f389fb26cac01e391852552
madalinaeleonorag <madalinaeleonora.gheorghe@gmail.com>
2019-09-24 23:20:30 +0300

    added black variable, search inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,26 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'UniversityRating';
+  firstPage: boolean;
+  
 
   constructor(private firebaseService: FirebaseService, private translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('ro');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    this.translate.setDefaultLang('ro');
+    this.translate.use('en');
   }
 
   ngOnInit() {
-    this.firebaseService.getBachelorsData().subscribe(result => console.log('getBachelorsData: ', result));
-    this.firebaseService.getCommentsData().subscribe(result => console.log('getCommentsData: ', result));
-    this.firebaseService.getDoctoralsData().subscribe(result => console.log('getDoctoralsData: ', result));
-    this.firebaseService.getFacultiesData().subscribe(result => console.log('getFacultiesData: ', result));
-    this.firebaseService.getMastersData().subscribe(result => console.log('getMastersData: ', result));
-    this.firebaseService.getRequestsData().subscribe(result => console.log('getRequestsData: ', result));
-    this.firebaseService.getUniversitiesData().subscribe(result => console.log('getUniversitiesData: ', result));
-    this.firebaseService.getUsersData().subscribe(result => console.log('getUsersData: ', result));
+    //TODO get active route for header styling
+
+
+    // this.firebaseService.getBachelorsData().subscribe(result => console.log('getBachelorsData: ', result));
+    // this.firebaseService.getCommentsData().subscribe(result => console.log('getCommentsData: ', result));
+    // this.firebaseService.getDoctoralsData().subscribe(result => console.log('getDoctoralsData: ', result));
+    // this.firebaseService.getFacultiesData().subscribe(result => console.log('getFacultiesData: ', result));
+    // this.firebaseService.getMastersData().subscribe(result => console.log('getMastersData: ', result));
+    // this.firebaseService.getRequestsData().subscribe(result => console.log('getRequestsData: ', result));
+    // this.firebaseService.getUniversitiesData().subscribe(result => console.log('getUniversitiesData: ', result));
+    // this.firebaseService.getUsersData().subscribe(result => console.log('getUsersData: ', result));
   }
 
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+export enum Categories {
+  University = 'UNIVERSITY',
+  Faculty = 'FACULTY',
+  Bachelor = 'BACHELOR',
+  Master = 'MASTER',
+  Doctoral = 'DOCTORAL'
+}
+
+export enum Sorting {
+  Ascending = 'ASCENDING',
+  Descending = 'DESCENDING',
+  Initial = 'NOSORT'
+}
+
+export enum InstitutionType {
+  Private = 'PRIVATE',
+  State = 'STATE'
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  categories: string[] = [];
+  sortTypes: string[] = [];
+  typeOfInstitution: string[] = [];
+
+  constructor() {
+    this.categories = Object.keys(Categories);
+    this.sortTypes = Object.keys(Sorting);
+    this.typeOfInstitution = Object.keys(InstitutionType);
+  }
 
   ngOnInit() {
   }
