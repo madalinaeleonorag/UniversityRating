a400169355f2435cb5be79ffae4beaf57a55b930
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2020-03-06 16:24:57 +0200

    added azure ai text analysis

---
+++ src/app/services/azure-ai-text-analysis.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { TextAnalyticsClient, TextAnalyticsApiKeyCredential } from '@azure/ai-text-analytics';
+
+const key = '2e1ffec2d1f347a787bec6dbbf17fe0a';
+const endpoint = `https://universityrating.cognitiveservices.azure.com/`;
+const client = new TextAnalyticsClient(endpoint, new TextAnalyticsApiKeyCredential(key));
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AzureAiTextAnalysisService {
+
+  constructor() { }
+
+  sentimentAnalysis() {
+
+    const sentimentInput = [
+      "this sucks"
+    ];
+    client.analyzeSentiment(sentimentInput).then(result => {
+      console.log(result[0])
+    });
+  }
+
+}
