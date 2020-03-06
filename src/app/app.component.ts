9c9f89d507ac2ee3b5535cd3504e14cdc67dc682
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2020-03-06 16:24:57 +0200

    added azure ai text analysis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { AzureAiTextAnalysisService } from './services/azure-ai-text-analysis.service';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,14 @@ export class AppComponent implements OnInit {
   title = 'UniversityRating';
   firstPage: boolean;
 
-  constructor(private translate: TranslateService, private router: Router, private authService: AuthService) {
+  constructor(private translate: TranslateService, private router: Router, private authService: AuthService, private textAnalysisService: AzureAiTextAnalysisService) {
     this.translate.setDefaultLang('ro');
     this.translate.use('en');
     this.router.events.subscribe(value => {
       router.url.toString() === '/' ? this.firstPage = true : this.firstPage = false;
     });
     this.authService.verifyIfUserAlreadySignedIn();
+    this.textAnalysisService.sentimentAnalysis();
   }
 
   ngOnInit() {
